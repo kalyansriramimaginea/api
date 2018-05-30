3caf537f50d076d25e370c6e20551c4ebc090f3d
Daniel Phillips <danp@propaganda3.com>
2018-05-30 12:14:18 -0500

    no message

--- app/Helpers/UA.php
+++ app/Helpers/UA.php
@@ -39,7 +39,7 @@ class UA
 
             foreach($request->get('targets') as $target) {
                 $installations = Installation::where('contactEmail', $target['filter-email']);
-                var_dump($installations);
+                var_dump($installations->count());
                 foreach($installations as $install) {
                     if($install->device == 'ios') {
                         if(!isset($audience['device_token'])) {
