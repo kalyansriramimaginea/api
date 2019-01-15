cc9ac17d9488b7c26588eb314a0f66b739a33f69
Kirubhakar Thangaraj <kirubhakar.thangaraj@jet.com>
2019-01-15 22:23:18 +0530

    Changing the DB config

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
         'mongodb' => [
             'driver'   => 'mongodb',
             'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
+            'port'     => 27017,
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
